450a5ad46643043b930b527a04b372ce17cb3c4f
djtoler <84260551+djtoler@users.noreply.github.com>
2021-11-23 11:14:54 -0500

    randnum

---
+++ script30.js
@@ -0,0 +1,28 @@
+const studentList = 
+["Logan****",
+"DJ****",
+"Steve****",
+"Nicolas****",
+"James****",
+"Momo****",
+"Alex****",
+"Tom****",
+"Kat****",
+"Narcis****",
+"Mark****"]
+
+const randomStudentList = [];
+function randomGenerator (alwaysZero, totalNumberOfStudents) {
+  for ( let i = 0; i <3; i++ ) {
+    let randomStudent = randomStudentList.push(Math.floor(Math.random() * totalNumberOfStudents) + alwaysZero);
+    let studentAppend = document.getElementById("name").append(studentList[randomStudentList[i]]);
+    console.log(studentList[randomStudentList[i]]);
+  }
+
+}
+
+randomGenerator(0, 11);
+
+// const randomThree = (Math.floor(Math.random() * studentList.length)) ;
+// console.log(studentList[randomThree], studentList[randomThree], studentList[randomThree]);
+
